17406cb4a0dd6a938c73b7212ac4132e23f2f175
DeekshithaRaja <deekshithadeekshu17@gmail.com>
2025-11-15 23:58:48 +0530

    Create LargestFixed.java

---
+++ LargestFixed.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class LargestFixed {
+    public static void main(String[] args) {
+        int a = 10, b = 25, c = 15;
+
+        if (a >= b && a >= c)
+            System.out.println("Largest: " + a);
+        else if (b >= a && b >= c)
+            System.out.println("Largest: " + b);
+        else
+            System.out.println("Largest: " + c);
+    }
+}
+public class Largest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
+        if (a >= b && a >= c)
+            System.out.println("Largest: " + a);
+        else if (b >= a && b >= c)
+            System.out.println("Largest: " + b);
+        else
+            System.out.println("Largest: " + c);
+        sc.close();
+    }
+}
