bd6884e3c0e91057f483404755a5df8848eba031
DeekshithaRaja <deekshithadeekshu17@gmail.com>
2025-11-15 23:33:29 +0530

    Create AboutMe.java

---
+++ AboutMe.java
@@ -0,0 +1,10 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        String name = "Deekshitha";
+        int age = 18;
+        String language = "Java";
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Favorite Language: " + language);
+    }
+}
