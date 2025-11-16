7fc206d31c3dd0cf61082518c0a6aa3b59653631
DeekshithaRaja <deekshithadeekshu17@gmail.com>
2025-11-16 14:35:38 +0530

    Update LargestFixed.java

--- LargestFixed.java
+++ LargestFixed.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 class LargestFixed {
     public static void main(String[] args) {
         int a = 10, b = 25, c = 15;
-
         if (a >= b && a >= c)
             System.out.println("Largest: " + a);
         else if (b >= a && b >= c)
